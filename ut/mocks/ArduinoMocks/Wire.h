b209dc30e024e29267ea9703760de84ba284f2ed
Lukasz Drozdz <lukasz.drozdz93@gmail.com>
2017-02-28 17:46:57 +0100

    Added mock definitions to WireMock

--- ut/mocks/ArduinoMocks/Wire.h
+++ ut/mocks/ArduinoMocks/Wire.h
@@ -1,63 +1,44 @@
 #ifndef __Wire_h__
 #define __Wire_h__
+
 #include <stdint.h>
 #include <gmock/gmock.h>
-#include <string.h>
 
 #define BUFFER_LENGTH 32
-#define I2C2 0x4819c000
-#define I2C1 0x00000000
-#define WIRE_INTERFACES_COUNT 1
 
-class Wire_ {
-  public:
-    void begin();
-    void begin(uint8_t);
-    void begin(int);
-    void beginTransmission(uint8_t);
-    uint8_t endTransmission(void);
-    uint8_t endTransmission(uint8_t);
-    uint8_t requestFrom(uint8_t, uint8_t);
-    uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
-    uint8_t write(uint8_t);
-    uint8_t write(char*);
-    uint8_t write(uint8_t, uint8_t);
-    uint8_t available(void);
-    uint8_t read(void);
-    void onReceive(uint8_t*);
-    void onRequest(uint8_t*);
-    inline size_t write(unsigned long n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(long n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(unsigned int n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(int n) {
-      return write((uint8_t)n);
-    }
+class WireMock
+{
+
+public:
+  MOCK_METHOD0(begin, void());
+  MOCK_METHOD1(begin, void(uint8_t));
+  MOCK_METHOD1(begin, void(int));
+  MOCK_METHOD1(beginTransmission, void(uint8_t));
+  MOCK_METHOD0(endTransmission, uint8_t(void));
+  MOCK_METHOD1(endTransmission, uint8_t(uint8_t));
+
+  MOCK_METHOD2(requestFrom, uint8_t(uint8_t, uint8_t));
+  MOCK_METHOD3(requestFrom, uint8_t(uint8_t, uint8_t, uint8_t));
+  MOCK_METHOD2(requestFrom, uint8_t(int, int));
+  MOCK_METHOD3(requestFrom, uint8_t(int, int, int));
+  MOCK_METHOD1(write, size_t(uint8_t));
+  MOCK_METHOD1(write, size_t(unsigned long));
+  MOCK_METHOD1(write, size_t(unsigned int));
+  MOCK_METHOD1(write, size_t(long));
+  MOCK_METHOD1(write, size_t(int));
+  MOCK_METHOD2(write, size_t(const uint8_t *, size_t));
+  MOCK_METHOD0(available, int());
+  MOCK_METHOD0(read, int());
+  MOCK_METHOD0(peek, int());
+  MOCK_METHOD0(flush, void());
+  MOCK_METHOD1(onReceive, void( void (*)(int) ));
+  MOCK_METHOD1(onRequest, void( void (*)(int) ));
+
 };
-extern Wire_ Wire;
 
-class WireMock {
-  public:
-    MOCK_METHOD0(begin, void());
-    MOCK_METHOD1(begin, void(uint8_t));
-    MOCK_METHOD1(begin, void(int));
-    MOCK_METHOD1(beginTransmission, void(uint8_t));
-    MOCK_METHOD0(endTransmission, uint8_t(void));
-    MOCK_METHOD1(endTransmission, uint8_t(uint8_t));
-    MOCK_METHOD2(requestFrom, uint8_t(uint8_t, uint8_t));
-    MOCK_METHOD3(requestFrom, uint8_t(uint8_t, uint8_t, uint8_t));
-    MOCK_METHOD1(write, uint8_t(uint8_t));
-    MOCK_METHOD1(write, uint8_t(char*));
-    MOCK_METHOD2(write, uint8_t(uint8_t, uint8_t));
-    MOCK_METHOD0(available, uint8_t(void));
-    MOCK_METHOD0(read, uint8_t(void));
-    MOCK_METHOD1(onReceive, void(uint8_t*));
-    MOCK_METHOD1(onRequest, void(uint8_t*));
+class WireMockInstanceWrapper
+{
+
 };
 
 WireMock* WireMockInstance();
