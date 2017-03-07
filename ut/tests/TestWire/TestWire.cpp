1ff5caa7b8cf76e0b8cabc2b1d102c42dbbd513f
Lukasz Drozdz <lukasz.drozdz93@gmail.com>
2017-03-07 20:17:22 +0100

    Refactoring, got working with some templates

--- ut/tests/TestWire/TestWire.cpp
+++ ut/tests/TestWire/TestWire.cpp
@@ -5,7 +5,7 @@
 #include <Wire.h>
 #include <functional>
 
-#define IMPLEMENT_FORWARDING_CALL_TO_MOCK(RETURN_TYPE, FUNCTION_NAME) \
+#define MOCK_GLOBAL_METHOD(RETURN_TYPE, FUNCTION_NAME) \
 template< typename... Args>\
 RETURN_TYPE FUNCTION_NAME (Args... args)\
 {\
@@ -13,95 +13,99 @@ RETURN_TYPE FUNCTION_NAME (Args... args)\
 }\
 
 
-class SampleMockImplementation
+#define REAL_MOCK_IMPLEMENTATION(MockName)\
+struct MockName##_Implementation\
+
+REAL_MOCK_IMPLEMENTATION(Serial)
 {
-public:
   MOCK_METHOD0(foo, int() );
   MOCK_METHOD1(foo, int(int) );
   MOCK_METHOD2(foo, int(int, int) );
+  MOCK_METHOD0(boo, int() );
+  MOCK_METHOD1(boo, int(int) );
+  MOCK_METHOD2(boo, int(int, int) );
 };
 
 
-template< typename MockImplementationTypename>
-class MockGlobalObjectTemplate
+#define GLOBAL_MOCK_IMPLEMENTATION(MockName)\
+struct MockName##_PointerClass\
+{\
+  MockName##_Implementation* mockPointer = nullptr;\
+};\
+struct MockName##_GlobalObjectMethodImplementation : public MockName##_PointerClass\
+
+GLOBAL_MOCK_IMPLEMENTATION(Serial)
 {
-  using ThisClassType = MockGlobalObjectTemplate<MockImplementationTypename>;
-  MockGlobalObjectTemplate& operator=(const MockGlobalObjectTemplate& other) = delete;
-  MockGlobalObjectTemplate(const MockGlobalObjectTemplate& other) = delete;
+  MOCK_GLOBAL_METHOD(int, foo)
+  MOCK_GLOBAL_METHOD(int, boo)
+};
+
+
+//#define GLOBAL_MOCK_DEFINISIONS(MockName)
+class MockGlobalObject : public Serial_GlobalObjectMethodImplementation
+{
+  MockGlobalObject& operator=(const MockGlobalObject& other) = delete;
+  MockGlobalObject(const MockGlobalObject& other) = delete;
 
 public:
-  MockGlobalObjectTemplate(){}
-  MockImplementationTypename* mockPointer = nullptr;
+  MockGlobalObject(){}
 
-  static ThisClassType& getGlobalInstanceReference()
+
+  static MockGlobalObject& getGlobalInstanceReference()
   {
-    static MockGlobalObjectTemplate globalInstance;
+    static MockGlobalObject globalInstance;
     return globalInstance;
   }
-  IMPLEMENT_FORWARDING_CALL_TO_MOCK(int, foo);
+
 };
 
 
-template<typename MockImplementationTypename,
-         typename MockGlobalObject = MockGlobalObjectTemplate<MockImplementationTypename> >
-class MockLocalObjectTemplate
+class MockLocalObject : public Serial_Implementation
 {
 
-  MockImplementationTypename mockImpl{};
   MockGlobalObject& globalObj = MockGlobalObject::getGlobalInstanceReference();
 
 public:
-  MockLocalObjectTemplate()
+  MockLocalObject()
   {
     EXPECT_EQ(nullptr, globalObj.mockPointer)
-      << "Are you trying to instantiate multiple local mock object in one scope?";
-    globalObj.mockPointer = &mockImpl;
+      << "Are you trying to instantiate multiple local mock objects in one scope?";
+    globalObj.mockPointer = this;
   }
 
-  ~MockLocalObjectTemplate()
+  ~MockLocalObject()
   {
     EXPECT_NE(nullptr, globalObj.mockPointer);
     globalObj.mockPointer = nullptr;
   }
-
-  MockImplementationTypename& operator*()
-  {
-    return mockImpl;
-  }
 };
-
-using MockGlobalObject = MockGlobalObjectTemplate<SampleMockImplementation>;
-using MockLocalObject = MockLocalObjectTemplate<SampleMockImplementation>;
-
 
 using ::testing::Return;
 using ::testing::_;
 
-
-
 auto& globalObj = MockGlobalObject::getGlobalInstanceReference();
-
-
 
 TEST(SomeMock, FirstTest)
 {
   MockLocalObject obj;
 
-  EXPECT_CALL(*obj, foo()).WillOnce(Return(1) );
-  EXPECT_CALL(*obj, foo(2)).WillOnce(Return(2) );
-  EXPECT_CALL(*obj, foo(3, 3)).WillOnce(Return(3) );
+  EXPECT_CALL(obj, foo()).WillOnce(Return(1) );
+  EXPECT_CALL(obj, foo(2)).WillOnce(Return(2) );
+  EXPECT_CALL(obj, foo(3, 3)).WillOnce(Return(3) );
+  EXPECT_CALL(obj, boo(3, 3)).WillOnce(Return(4) );
 
   EXPECT_EQ(1, globalObj.foo());
   EXPECT_EQ(2, globalObj.foo(2));
   EXPECT_EQ(3, globalObj.foo(3, 3));
+  EXPECT_EQ(4, globalObj.boo(3, 3));
 }
 TEST(SomeMock, SecondTest)
 {
   MockLocalObject obj;
 
-  EXPECT_CALL(*obj, foo()).WillOnce(Return(1) );
-  EXPECT_CALL(*obj, foo(2)).WillOnce(Return(2) );
-  EXPECT_CALL(*obj, foo(3, 3)).WillOnce(Return(3) );
+  EXPECT_CALL(obj, foo()).WillOnce(Return(1) );
+  EXPECT_CALL(obj, foo(2)).WillOnce(Return(2) );
+  EXPECT_CALL(obj, foo(3, 3)).WillOnce(Return(3) );
 
   EXPECT_EQ(1, globalObj.foo());
   EXPECT_EQ(2, globalObj.foo(2));
