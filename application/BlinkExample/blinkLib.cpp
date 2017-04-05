c4f0a8eb1f4e8491103ef5958dd9d96b30264f59
Lukasz Drozdz <lukasz.drozdz93@gmail.com>
2017-04-05 20:33:41 +0200

    Refactored blinklib, added SPI mock, refactored cmake

--- application/BlinkExample/blinkLib.cpp
+++ application/BlinkExample/blinkLib.cpp
@@ -1,29 +1,14 @@
 #include <Arduino.h>
 
-#include <blinkLib.h>
+#include "blinkLib.h"
+#include <Wire.h>
+#include <SPI.h>
 
 
 void blink_setup(uint8_t pin) {
   pinMode(pin, OUTPUT);
 }
 
-void koo1(int a)
-{
-
-}
-void koo2(long a)
-{
-
-}
-void koo3(unsigned long a)
-{
-  koo1(a);
-}
-
-void koo4(long a)
-{
-  koo3(a);
-}
 
 void blink(unsigned long duration, uint8_t pin) {
   digitalWrite(pin, HIGH);   // set the LED on
@@ -31,6 +16,4 @@ void blink(unsigned long duration, uint8_t pin) {
   digitalWrite(pin, LOW);    // set the LED off
   delay(duration);           // wait for a second
 }
-
-
 
--- arduino-cmake/example/blink.cpp
+++ arduino-cmake/example/blink.cpp
@@ -1,33 +1,6 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-  Example uses a static library to show off generate_arduino_library().
- 
-  This example code is in the public domain.
- */
-#if ARDUINO >= 100
-    #include "Arduino.h"
-#else
-    #include "WProgram.h"
-#endif
-
-#include "blink_lib.h"
-#include <BaseClass.h>
-
-class Derived : public TeaBreak
-{
-protected:
-  int makeCoffee(bool milk, double sugars) override
-  {
-    return milk ? sugars : -sugars;
-  }
-
-  int makeHerbalTea()
-  {
-    return 100;
-  }
-};
-
+#include "Arduino.h"
+#include <SPI.h>
+#include <blinkLib.h>
 
 
 void setup() {                
